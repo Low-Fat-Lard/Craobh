f118bc20bd95c431acd479b6b217b8d315cbe0ae
Low fat lard <94632232+Low-Fat-Lard@users.noreply.github.com>
2023-07-29 10:09:30 +0900

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+async function navigateTo(page) {
+  const content = document.getElementById('content');
+  let htmlContent = '';
+
+  try {
+    const response = await fetch(`../html/${page}.html`);
+    htmlContent = await response.text();
+  } catch (error) {
+    htmlContent = '<h1>Page Not Found</h1><p>The requested page was not found.</p>';
+  }
+
+  content.innerHTML = htmlContent;
+}
